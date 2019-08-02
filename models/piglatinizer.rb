8557c7b2756f6b8c3563c25fdbd375e34a8b0a5a
Conor Owens <conorj.owens@gmail.com>
2019-08-02 21:14:28 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,14 +1,18 @@
 class PigLatinizer
+  
 
   def piglatinize(user_phrase)
-    @new_word = user_phrase.split("")
-    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-    if vowels.include?(@new_word[0])
-      latinized_word = begins_with_vowel.join("")
+    if user_phrase.split(" ").count > 1 
+      user_phrase.to_pig_latin
     else
-      latinized_word = begins_with_consonant.join("")
-    end
-    latinized_word
+      @new_word = user_phrase.split("")
+      vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+      if vowels.include?(@new_word[0])
+        latinized_word = begins_with_vowel.join("")
+      else
+        latinized_word = begins_with_consonant.join("")
+      end
+      latinized_word
   end
 
   def to_pig_latin(sentence)
