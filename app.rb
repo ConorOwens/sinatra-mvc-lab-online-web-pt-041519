7207bc4c1b6b31cd212e791a93be3663d2b790f3
Conor Owens <conorj.owens@gmail.com>
2019-08-02 21:10:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,9 +7,7 @@ class App < Sinatra::Base
   end
   
   post '/piglatinize' do
-    user_word = params["user_phrase"]
-    word = PigLatinizer.new
-    @pig_latinized_word = word.to_pig_latin(user_word)
+    @pig_latinized_word = PigLatinizer.new.to_pig_latin(params[:user_phrase])
 
     erb :oinked
   end
